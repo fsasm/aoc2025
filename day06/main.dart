ed144db2a46d1f2fdfa94cb31ba2d1cdc73b644e
Fabjan Sukalia <fsukalia@gmail.com>
2025-12-06 19:16:33 +0100

    solution for day06-part2

--- day06/main.dart
+++ day06/main.dart
@@ -42,11 +42,59 @@ void part1(List<String> lines) {
 void part2(List<String> lines) {
   int sum = 0;
 
+  List<List<String>> digits = <List<String>>[];
+  String opLine = "";
+
+  // go down every column and capture it in a list
+  // This is like the transpose for a matrix but with the right-to-left quirk and
+  // that some digits are empty space
+  for (int i = 0; i < lines[0].length; i++) {
+    List<String> d = <String>[];
+    for (final l in lines) {
+      if (l.contains("*") || l.contains("+")) {
+        opLine = l;
+        break;
+      }
+
+      if (l[i] == " ") {
+        continue;
+      }
+
+      d.add(l[i]);
+    }
+
+    digits.add(d);
+  }
+
+  final ops = opLine
+      .split(" ")
+      .map((s) => s.trim())
+      .where((s) => s.isNotEmpty)
+      .toList();
+
+  int digitIndex = digits.length - 1;
+  for (final op in ops.reversed) {
+    int result = (op == "*") ? 1 : 0;
+    // use every column until we hit an empty column. The empty column gets skipped
+    // after the loop, so that the next operation begins with a valid column.
+    while (digitIndex >= 0 && digits[digitIndex].isNotEmpty) {
+      int n = digits[digitIndex].map(int.parse).fold(0, (p, v) => 10 * p + v);
+      if (op == "*") {
+        result *= n;
+      } else {
+        result += n;
+      }
+      digitIndex--;
+    }
+    digitIndex--;
+    sum += result;
+  }
+
   print("Part 2: $sum");
 }
 
 void main() {
-  var f = File("input");
+  var f = File("example");
   var lines = f.readAsLinesSync();
 
   part1(lines);
